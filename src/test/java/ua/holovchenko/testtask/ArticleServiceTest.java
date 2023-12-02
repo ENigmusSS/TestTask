f7eb379bffafca5e61c3fa0f451fc8eb3684acd7
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2023-12-02 18:50:47 +0200

    some tests

--- src/test/java/ua/holovchenko/testtask/ArticleServiceTest.java
+++ src/test/java/ua/holovchenko/testtask/ArticleServiceTest.java
@@ -9,15 +9,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import ua.holovchenko.testtask.converter.ArticleConverter;
 import ua.holovchenko.testtask.entity.ArticleEntity;
 import ua.holovchenko.testtask.model.Article;
 import ua.holovchenko.testtask.repository.ArticleEntityRepository;
 import ua.holovchenko.testtask.service.ArticleService;
-import org.junit.jupiter.api.Assertions;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ArticleServiceTest {
@@ -41,7 +43,47 @@ public class ArticleServiceTest {
     void getArticles_noArgs_returnsValidData() {
         Mockito.when(this.repo.findAll(PageRequest.of(0,10))).thenReturn(testArticleEntities);
 
-        Assertions.assertEquals(this.service.getArticles(null,null), testArticles);
+        assertEquals(this.service.getArticles(null,null), testArticles);
+    }
+
+    @Test
+    void getArticles_validArgs_returnsValidData() {
+        Mockito.when(this.repo.findAll(PageRequest.of(0,3))).thenReturn(testArticleEntities);
+
+        assertEquals(this.service.getArticles(1,3), testArticles);
+    }
+
+    @Test
+    void getArticles_invalidArgs_returnsValidData() {
+        Mockito.when(this.repo.findAll(PageRequest.of(0,10))).thenReturn(testArticleEntities);
+
+        assertEquals(this.service.getArticles(-7, -34), testArticles);
+    }
+
+    @Test
+    void createArticle_validArgs_returnsValidData() {
+        Article article = testArticles.get(0);
+        ArticleEntity entity = ArticleConverter.toEntity(article);
+        Mockito.when(this.repo.save(entity)).thenReturn(entity);
+
+        assertEquals(article, this.service.createArticle(article));
+    }
+
+    @Test
+    void createArticle_invalidArgs_throwsCorrectException() {
+        String invalidTitle = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa";
+
+        assertThrows(IllegalArgumentException.class, () -> this.service.createArticle(new Article( invalidTitle, "Testerenko", "comment", LocalDate.now())));
+        assertThrows(IllegalArgumentException.class, () -> this.service.createArticle(new Article()));
+    }
+
+    @Test
+    void getStatistics_returnValidData() {
+        long[] ls = {1L,1L,1L,1L,1L,1L,1L};
+        for (int i = 0; i < 7; i++) {
+            Mockito.when(this.repo.countByPublished(Date.valueOf(LocalDate.now().minusDays(i)))).thenReturn(1L);
+            assertEquals(ls[i], this.service.getStatistics(7)[i]);
+        }
     }
 
 }
