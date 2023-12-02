9e17505131a9332caede183821058b75d40e7bc0
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2023-12-03 00:45:38 +0200

    some tests

--- src/test/java/ua/holovchenko/testtask/ArticleIT.java
+++ src/test/java/ua/holovchenko/testtask/ArticleIT.java
@@ -15,12 +15,14 @@ import ua.holovchenko.testtask.repository.ArticleEntityRepository;
 import java.sql.Date;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
-@WithUserDetails("admin")
+@WithUserDetails()
 public class ArticleIT {
     @Autowired
     MockMvc mockMvc;
@@ -63,6 +65,86 @@ public class ArticleIT {
                             ]
                         """
                         )
+                );
+        this.repo.deleteAll();
+    }
+
+    @Test
+    void  createArticle_PayloadValid_ReturnsValidResponseEntity() throws Exception{
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                            {
+                                    "title" : "Test1",
+                                    "author" : "Testerov",
+                                    "content" : "Test1",
+                                    "published" : "2023-02-11"
+                            }
+                        """);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json(
+                                """
+                            {
+                                    "title" : "Test1",
+                                    "author" : "Testerov",
+                                    "content" : "Test1",
+                                    "published" : "2023-02-11"
+                            }
+                        """
+                        )
+                );
+        assertEquals(new ArticleEntity("Test1", "Testerov", "Test1", Date.valueOf("2023-02-11")), this.repo.findAll().get(0));
+    this.repo.deleteAll();
+    }
+
+    @Test
+    void  createArticle_PayloadNotValid_ReturnsValidResponseEntity() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                            {
+                                    "title" : "",
+                                    "author" : "Testerov",
+                                    "content" : "",
+                                    "published" : "2023-02-11"
+                            }
+                        """);
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(
+                        status().isBadRequest()
+                );
+        assertTrue(this.repo.findAll().isEmpty());
+    }
+
+    @Test
+    void getStatistics_noArgument_ReturnsValidResponseEntity() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/articles/stats");
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                    status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json("""
+                            [ 0, 0, 0 ,0 ,0, 0, 0]
+                            """)
+                );
+    }
+
+    @Test
+    void getStatistics_withArgument_ReturnsValidResponseEntity() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/articles/stats").param("forDays", "3");
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentType(MediaType.APPLICATION_JSON),
+                        content().json("""
+                            [0,0,0]
+                            """)
                 );
     }
 }
